0dbea59034806e993e9427606039decd6ab5fca9
appsstars <appsstars2019gmail.com>
2020-04-28 11:08:17 -0500

    update

--- nosotros.php
+++ nosotros.php
@@ -1,13 +1,29 @@
 <?php require_once 'layouts/header.php'; ?>
-<div class="contenido">
+<div class="nosotros">
 	<div class="section-about">
-		<div class="contaisner">
+		<div class="container">
 			<div class="row" id="section-about">
 				<div class="col-xs-12 col-md-6">
 					<div class="pull-right" style="max-width: 500px">
 						<h1 class="text-primary"><b>Quienes somos</b></h1>
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, saepe. Dolores aliquam eveniet dolorum laudantium velit, eius labore nulla ut? Autem dolor delectus dolorum quibusdam quae iure in, maiores voluptas!</p>
+						<p>
+						   AppsStars es una empresa joven, liderada por un grupo de emprendedores que, 
+						   combinando fortalezas en desarrollo de software, diseño web, áreas tecnológicas y 
+						   Tic’s en general, se está posesionando en el mercado Nacional compitiendo 
+						   con calidad, cumplimiento y responsabilidad.
+						</p>
 						<a href="" class="btn btn-outline-primary">Quiero una cotización</a>
+					</div>
+					<div class="pull-right" style="max-width: 500px">
+						<h1 class="text-primary"><b>Nuestros Valores</b></h1>
+						<p>
+							El trabajo del día a día de nuestro equipo está basado en tres valores, confianza, 
+							profesionalidad e integridad. Nuestra filosofía es transformar con tecnología las 
+							empresas. Los clientes son lo más importante para APPSSTARS. La innovación es otro 
+							de los ejes centrales de esta compañía que ofrece soluciones ágiles, globales y 
+							personalizadas a cada empresa.
+						</p>
+						
 					</div>
 					
 				</div>
@@ -24,16 +40,27 @@
 			  		<h1 class="text-center text-info"><b>AGENCIA</b></h1>
 			  	</div>
 			    <div class="col-md-4">
-			      <div class="card card-1">
+			      <div class="card card-1"> h
 			        <h3>MISIÓN</h3>
-			        <p>A curated set of   ES5/ES6/TypeScript wrappers for plugins to easily add any native functionality to your Ionic apps.</p>
+						<p>
+							La misión de APPSSTARS se focaliza en la creación y gestión de servicios TIC para acercar
+							la tecnología y la innovación a las empresas para acelerar y automatizar sus procesos. 
+							Transformar digitalmente y hacer más accesibles los procesos tecnológicos a todas las personas. 
+							Con APPSSTARS siempre tendrá un trato personal, cercano y directo, disponiendo siempre de la 
+							flexibilidad adecuada a sus necesidades. Nuestra empresa siempre le acompañará buscando 
+							el camino de la excelencia personal y profesional.
+						</p>
 			      </div>
 			    </div>
-			    <div class="col-md-4">
+			    <div class="col-md-4 ">
 			      <div class="card card-2">
 			        <h3>VISION</h3>
-			        <p>Tabs, buttons, inputs, lists, cards, and more! A comprehensive library
-			          of mobile UI components, ready to go.</p>
+						<p>
+							Ser un referente en el mercado nacional en el sector de las TIC y 
+							llevarla a ser una empresa tecnológica referente en la transformación digital, 
+							innovadora y líder en nuestro sector. Seguir ofreciendo soluciones globales, 
+							servicios informáticos y generación de valor a todas las empresas.
+						</p>
 			      </div>
 			    </div>
 			    <div class="col-md-4">
@@ -46,7 +73,9 @@
 			  </div>
 			</div>
 		</section>
+
+		
 	</div>
 </div>
 	
-	
+<?php require_once 'layouts/footer.php'; ?>
